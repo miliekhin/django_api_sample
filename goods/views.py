c9b82976e3be2d462f92dd4e27c3cd7ba6e6f388
ALEXIOS <lanvisor>
2021-03-22 14:49:37 +0300

    Добавлен класс - глобальный обработчик исключений для всех вьюшек

--- goods/views.py
+++ goods/views.py
@@ -9,59 +9,67 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import QueryDict
 
 
+class ViewsExceptionsHandler(View):
+    """ Глобальный обработчик исключений """
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            response = super().dispatch(request, *args, **kwargs)
+        except Exception as e:
+            return self._response({'error': str(e)}, status=400)
+
+        if isinstance(response, (dict, list)):
+            return self._response(response)
+        else:
+            return response
+
+    @staticmethod
+    def _response(data, *, status=200):
+        return JsonResponse(
+            data,
+            status=status,
+            safe=not isinstance(data, list),
+            json_dumps_params={'ensure_ascii': False}
+        )
+
+
 @method_decorator(csrf_exempt, name='dispatch')
-class ProductsView(View):
+class ProductsView(ViewsExceptionsHandler):
     """
     Отдача списка всех товаров с фильтрацией по имени поля в параметрах запроса.
     Создание товара.
     """
     def get(self, request):
         response = []
-        if not request.GET:
+        if not request.GET:  # Если нет параметров в запросе
             response = all_goods()
         else:
-            try:
-                for p in Product.objects.filter(**get_filters(request.GET)):
-                    response.append(product_as_dict(p))
-            except Exception as exc:
-                return JsonResponse({'error': str(exc)}, status=400)
+            for p in Product.objects.filter(**get_filters(request.GET)):
+                response.append(product_as_dict(p))
 
         return JsonResponse(response, status=200, safe=False)
 
     def post(self, request):
-        try:
-            product = create_product(request.POST)
-        except Exception as exc:
-            return JsonResponse({'error': str(exc)}, status=400)
+        product = create_product(request.POST)
         return JsonResponse(product_as_dict(product), status=201, safe=False)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class ProductsByIdView(View):
+class ProductsByIdView(ViewsExceptionsHandler):
     """Отдача товара по его id, редактирование, удаление. """
     def get(self, request, id):
-        try:
-            p = Product.objects.get(pk=id)
-        except ObjectDoesNotExist as exc:
-            return JsonResponse({'error': str(exc)}, status=400)
+        p = Product.objects.get(pk=id)
         return JsonResponse(product_as_dict(p), status=200, safe=False)
 
     def put(self, request, id):
-        try:
-            product = edit_product(QueryDict(request.body), id)
-        except Exception as exc:
-            return JsonResponse({'error': str(exc)}, status=400)
+        product = edit_product(QueryDict(request.body), id)
         return JsonResponse(product_as_dict(product), status=200, safe=False)
 
     def delete(self, request, id):
-        try:
-            Product.objects.filter(pk=id).update(is_deleted=True)
-        except ObjectDoesNotExist as exc:
-            return JsonResponse({'error': str(exc)}, status=400)
+        Product.objects.filter(pk=id).update(is_deleted=True)
         return JsonResponse({}, status=200)
 
 
-class ProductsByNameView(View):
+class ProductsByNameView(ViewsExceptionsHandler):
     """Отдача списка всех товаров по совпадению с именем"""
     def get(self, request, name):
         resp = []
@@ -71,7 +79,7 @@ class ProductsByNameView(View):
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class CategoriesView(View):
+class CategoriesView(ViewsExceptionsHandler):
     """Отдача списка всех категорий и создание категории"""
     def get(self, request):
         all_categories = Category.objects.all()
@@ -82,33 +90,21 @@ class CategoriesView(View):
         return JsonResponse(resp, status=200, safe=False)
 
     def post(self, request):
-        try:
-            new_category, is_created = Category.objects.get_or_create(name=request.POST['name'])
-        except Exception as exc:
-            return JsonResponse({'error': str(exc)}, status=400)
-
+        new_category, is_created = Category.objects.get_or_create(name=request.POST['name'])
         return JsonResponse(model_to_dict(new_category), status=201, safe=False)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class CategoriesByIdView(View):
+class CategoriesByIdView(ViewsExceptionsHandler):
     """Отдача и удаление категории по id"""
     def get(self, request, id):
-        try:
-            c = Category.objects.get(pk=id)
-        except ModuleNotFoundError as exc:
-            return JsonResponse({'error': str(exc)}, status=400)
-
+        c = Category.objects.get(pk=id)
         return JsonResponse(model_to_dict(c), status=200, safe=False)
 
     def delete(self, request, id):
-        try:
-            c = Category.objects.get(pk=id)
-            if not Product.objects.filter(category__pk=c.pk).exists():
-                c.delete()
-            else:
-                return JsonResponse({'error': 'Unable to delete category. It has a relations.'}, status=400)
-
-        except ObjectDoesNotExist as exc:
-            return JsonResponse({'error': str(exc)}, status=400)
+        c = Category.objects.get(pk=id)
+        if not Product.objects.filter(category__pk=c.pk).exists():
+            c.delete()
+        else:
+            return JsonResponse({'error': 'Unable to delete category. It has a relations.'}, status=400)
         return JsonResponse({}, status=200)
